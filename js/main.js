01985e86632d58f7f5bb153fb2294665f8832b3e
edena <djaziz12393@gmail.com>
2023-10-13 10:17:52 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function changeColor() {
+  let container = document.getElementById("main-container");
+  debugger;
+  container.style.backgroundColor = container.style.backgroundColor === 'rgb(220, 250, 251)'
+    ? '#FFFFFF'
+    : '#DCFAFB';
+}
